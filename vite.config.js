77bc71c0c1e2521f5f42e70acbdede48c3b2da9f
Adrian <animacuba@gmail.com>
2025-09-05 21:17:43 +0200

    delete_avatar

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Neuraltax_test/',
   plugins: [react()],
   server: {
     proxy: {
